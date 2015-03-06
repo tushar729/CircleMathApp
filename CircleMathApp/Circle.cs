f33fb8ab6b112667ebedd3be72e25917b2fd2e32
tushar729 <kingtushar7772gmail.com>
2015-03-06 14:00:37 +0600

    circle

---
+++ CircleMathApp/Circle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleMathApp
+{
+    class Circle
+    {
+        private const double PI = 3.1416;
+
+        public double GetDiameter(double radius)
+        {
+            return 2 * radius;
+        }
+
+        public double GetPerimeter(double radius)
+        {
+            return 2*PI*radius;
+        }
+
+        public double GetArea(double radius)
+        {
+            return PI*radius*radius;
+        }
+    }
+}
---
+++ CircleMathApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleMathApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle aCircle = new Circle();
+            Console.WriteLine("Please Enter Radius of the Circle: ");
+            double radius = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Diameter of the circle: " + aCircle.GetDiameter(radius));
+            Console.WriteLine("Perimeter of the circle: " + aCircle.GetPerimeter(radius));
+            Console.WriteLine("Area of the circle: " + aCircle.GetArea(radius));
+            Console.ReadKey();
+        }
+    }
+}
